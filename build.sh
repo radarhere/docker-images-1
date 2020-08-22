076056d95bdf571d0eb56342a45fbb386739e2ef
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2020-08-22 16:59:53 +1000

    again

--- build.sh
+++ build.sh
@@ -3,12 +3,20 @@
 # Stop at any error, show all commands
 set -ex
 
-echo "TORCH"
-sudo apt-get update
-echo "FINISHED UPDATE 2"
+echo "BEFORE UPDATE"
 docker --version
-echo "TORCH INSTALL"
-sudo apt-get install docker-ce-cli
+echo "TORCHA"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "TORCHB"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo "TORCHC"
+sudo apt-get update
+echo "TORCHD"
+apt-cache policy docker-ce
+echo "TORCHE"
+apt-get install -y docker-ce
+echo "TORCHF"
+
 echo "FINISHED UPDATE 2"
 docker --version
 docker build --rm -t multibuild/xenial_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
