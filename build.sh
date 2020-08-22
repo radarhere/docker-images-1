d4a5ed9bddecc53544cadf88a26b6e30b58cd3fd
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2020-08-22 15:54:49 +1000

    version

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 # Stop at any error, show all commands
 set -ex
 
+echo "TORCH"
+docker --version
 docker build --rm -t multibuild/xenial_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
