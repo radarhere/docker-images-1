c3e7bb7b62dcca7da5d3e90455e99dabba17141c
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2020-08-22 17:08:09 +1000

    tee

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ echo "BEFORE UPDATE"
 docker --version
 echo "TORCHA"
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable" | tee /etc/apt/sources.list.d/docker.list
 echo "TORCHC"
 sudo apt-get update
 echo "TORCHD"
