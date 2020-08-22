d5ca16171f8c086d67b96a1999f08050621398db
Andrew Murray <3112309+radarhere@users.noreply.github.com>
2020-08-22 16:56:16 +1000

    cli

--- build.sh
+++ build.sh
@@ -5,12 +5,10 @@ set -ex
 
 echo "TORCH"
 sudo apt-get update
-echo "FINISHED UPDATE"
-apt-cache madison docker-ce
 echo "FINISHED UPDATE 2"
 docker --version
 echo "TORCH INSTALL"
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce-cli
 echo "FINISHED UPDATE 2"
 docker --version
 docker build --rm -t multibuild/xenial_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
